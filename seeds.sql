ac140b754e9fb29d1cf3598317801ce547549b02
Stephanie <whatawhat@hotmail.com>
2021-07-14 11:17:13 -0400

    deleted the employees and only kept the managers and the code worked

--- seeds.sql
+++ seeds.sql
@@ -27,8 +27,4 @@ INSERT INTO employee
 ("John", "Norris", 3, null),
 ("Thomas", "Alvarez", 6, null),
 ("Melissa", "Edwards", 7, null),
-("Sally", "Jordan", 1, null),
-("Tom", "Jones", 2, 4),
-("Daniel", "Porter", 4, 2),
-("Joshua", "Marshall", 5, 3),
-("Sabrina", "Mills", 5, 3);
+("Sally", "Jordan", 1, null);
