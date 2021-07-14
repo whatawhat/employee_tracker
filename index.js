c2a9f548088a243c6c244d74585443ce74dc563a
Stephanie <whatawhat@hotmail.com>
2021-07-14 19:32:34 -0400

    added function to ask questions and used switch to change where to go

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+//Dependencies
 const mysql = require ('mysql');
 const inquirer = require('inquirer');
 const table = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: '',
+    database: 'employeetrack_db',
+  });
+  
+  connection.connect(function (err) {
+      if (error) throw error;
+      console.log("Connected as id " + connection.threadId + "\n");
+      menu();
+  })
+  function menu() {
+      inquirer.prompt({
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menuChoice',
+        choices: ['Add Employee', 'Add department', 'Add role', 'View Employees', 'View departments', 'View Roles', 'Update Employee Role', 'Exit'],
+      }).then(answers => {
+          switch (answers.menuChoice) {
+              case "Add Employee":
+                  addEmployee()
+                  break;
+            case "Add department":
+                addDepartment()
+                break;
+            case "Add role":
+                addRole()
+                break;
+            case "View Employees":
+                viewEmployees()
+                break;
+            case "View departments":
+                viewDepartments()
+                break;
+            case "View Roles":
+                viewRoles()
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole()
+                break;
+            case "Exit":
+                break;
+            default:
+                connection.end()
+                break;
+          }
+      })
+  }
+
+  
+
+
+
